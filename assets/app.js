ef5b9e57c4d60409edaaba99f97bf7e9bfeda598
Tom9603 <tom.ochietti@gmail.com>
2025-09-25 23:41:51 +0200

    #9 Menu hamburger

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@
  * which should already be in your base.html.twig.
  */
 import './styles/scss/main.scss';
+import './js/menu.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
