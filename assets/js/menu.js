a699f7af990f70197f98ad33c21b9ca3440ef486
Tom9603 <tom.ochietti@gmail.com>
2025-09-29 17:11:56 +0200

    #11 CSS en viewport rajouté

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,15 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const closeBtn = document.getElementById("close-menu");
     const menu = document.getElementById("side-menu");
     const overlay = document.querySelector(".overlay");
+    const body = document.body;
 
     function openMenu() {
         menu.classList.add("open");
         overlay.classList.add("active");
+        body.classList.add("no-scroll");
     }
 
     function closeMenu() {
         menu.classList.add("closing");
         overlay.classList.remove("active");
+        body.classList.remove("no-scroll");
 
         menu.addEventListener("transitionend", () => {
             menu.classList.remove("open", "closing");
